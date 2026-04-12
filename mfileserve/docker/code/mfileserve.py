33e05c81d5a5ab9a80669f1e026c4ddd3459a9e0
Andrew Christ <andrew@openmarmot.com>
2026-04-12 14:14:02 -0700

    update

--- mfileserve/docker/code/mfileserve.py
+++ mfileserve/docker/code/mfileserve.py
@@ -76,4 +76,4 @@ def files(filename):
     return send_from_directory(DOWNLOAD_FOLDER, filename)
 
 if __name__ == '__main__':
-    app.run(debug=True, host='0.0.0.0', port=80)
+    app.run(debug=True, host='0.0.0.0', port=5000)
